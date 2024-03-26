a8efabe1b604ccb765211591b1f7355497fdad3b
Casper <casper.bjorkman@gmail.com>
2024-03-26 14:17:27 +0100

    Very basic company "login" and home page

--- backend/foretag/urls.py
+++ backend/foretag/urls.py
@@ -1,1 +1,7 @@
-urlpatterns = []
+from . import views
+from django.urls import path
+
+urlpatterns = [
+    path("", views.company_login),
+    path("<slug:name>/", views.company_home_page, name="company_home_page"),
+]
--- backend/foretag/views.py
+++ backend/foretag/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpRequest, Http404
+from .models import Store
+
 
 # Create your views here.
+def company_login(request: HttpRequest):
+    if request.method == "POST":
+        name = request.POST["name"]
+        try:
+            store = Store.objects.get(name=name)
+            return redirect("company_home_page", name=store.slug_name)
+        except Store.DoesNotExist:
+            pass
+
+    return render(request, "foretag/login_page.html")
+
+
+def company_home_page(request: HttpRequest, name: str):
+    # TODO check if name exists and check if logged in
+
+    return render(request, "foretag/home_page.html", {"name": name})
