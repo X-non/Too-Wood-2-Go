978b712c8c145aad974482daa93de796b868fa0d
Cecilia Lindeborg <cecilialindeborg@Cecilias-MacBook-Air.local>
2024-04-10 17:57:04 +0200

    color, styles and home widgets

---
+++ frontend/lib/constants/ew_colors.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+abstract final class EWColors {
+  static const Color primary = Color(0XFF7F8F6E);
+
+  static const Color lightgreen = Color(0XFFADB191);
+
+  static const Color darkgreen = Color(0XFF3B4233);
+
+  static const Color darkbrown = Color(0XFF6C593D);
+
+  static const Color brown = Color(0XFFC4A580);
+
+  static const Color lightbrown = Color(0XFFDACBB5);
+
+  static const Color beige = Color(0XFFDBD8D0);
+}
--- frontend/lib/pages/shoppingcart_page.dart
+++ frontend/lib/pages/shoppingcart_page.dart
@@ -1,4 +1,5 @@
 import 'package:eatwise/constants/EW_styles.dart';
+import 'package:eatwise/constants/ew_colors.dart';
 import 'package:flutter/material.dart';
 
 class ShoppingCartPage extends StatelessWidget {
@@ -6,7 +7,10 @@ class ShoppingCartPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-        child: Text('Cart Page', style: EWTextStyles.largeTitle));
+    return Center(
+        child: Text(
+      'Cart Page',
+      style: EWTextStyles.largeTitle.copyWith(color: EWColors.darkgreen),
+    ));
   }
 }
