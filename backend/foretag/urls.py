73ef1390fa30c95d612dea27cb38d1fd9906bcd9
LiamAnderberg <anderberg.liam@icloud.com>
2024-04-10 11:30:27 +0200

    Initial Prototype

--- backend/foretag/urls.py
+++ backend/foretag/urls.py
@@ -2,6 +2,8 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path("", views.company_login),
-    path("<slug:name>/", views.company_home_page, name="company_home_page"),
+    path("", views.company_login, name="company_login"),
+    path("home/<slug:name>/", views.company_home_page, name="company_home_page"),
+    path("createad/<slug:name>/", views.create_ad, name="create_ad"),
+    path("register/", views.company_register, name="company_register")
 ]
--- backend/foretag/views.py
+++ backend/foretag/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpRequest, Http404
-from .models import Store
+from .models import Store, Ad
+from .forms import StoreForm, AdForm
+from django.template.defaultfilters import slugify
+from django.urls import reverse
 
 
-# Create your views here.
 def company_login(request: HttpRequest):
     if request.method == "POST":
         name = request.POST["name"]
@@ -11,12 +13,55 @@ def company_login(request: HttpRequest):
             store = Store.objects.get(name=name)
             return redirect("company_home_page", name=store.slug_name)
         except Store.DoesNotExist:
-            pass
-
-    return render(request, "foretag/login_page.html")
+            return render(request, "foretag/login_page.html", {'nameExists': False})
+    return render(request, "foretag/login_page.html", {'nameExists': True})
 
 
 def company_home_page(request: HttpRequest, name: str):
     # TODO check if name exists and check if logged in
+    store = get_object_or_404(Store, slug_name=name)
+    context = applyFilters(request, store)
+    return render(request, "foretag/home_page.html", context)
 
-    return render(request, "foretag/home_page.html", {"name": name})
+def applyFilters(request: HttpRequest, store):
+    ads = Ad.objects.all()
+    ads = ads.filter(store=store.pk)
+    name_contains_queury = request.GET.get('search')
+    print(name_contains_queury)
+    if name_contains_queury != '' and name_contains_queury is not None:
+        ads = ads.filter(title__icontains=name_contains_queury)
+
+    context = {
+        'queryset': ads,
+        'store': store,
+        'search': name_contains_queury
+    }
+    return context
+    
+
+
+def company_register(request: HttpRequest):
+    if request.method == 'POST':
+        form = StoreForm(request.POST)
+        print(form)
+        if form.is_valid():
+            form.save()
+            return redirect('company_login')
+        else:
+            return render(request, 'foretag/company_registration.html', {'form': form}) #TODO {'form': form, 'had_error': True}
+    else:
+        form = StoreForm()
+    return render(request, "foretag/company_registration.html", {'form': form}) #TODO {'form': form, 'had_error': False}
+
+def create_ad(request: HttpRequest, name: str):
+    if request.method == 'POST':
+        form = AdForm(request.POST)
+        if form.is_valid():
+            form.save()  
+            url = reverse('company_home_page', kwargs={'name': name})
+            return redirect(url)
+        else:
+            return render(request, 'foretag/create_ad.html', {'form': form})
+    else:
+        form = AdForm()
+        return render(request, 'foretag/create_ad.html', {'form': form})
