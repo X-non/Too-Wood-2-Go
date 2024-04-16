329d25bec5bf7a44f1ebfbcbc682d41ee270e134
anec4889 <anton.eckervald.4889@student.uu.se>
2024-04-16 12:11:04 +0200

    pushing

--- frontend/lib/widgets/ew_company_container.dart
+++ frontend/lib/widgets/ew_company_container.dart
@@ -1,7 +1,9 @@
 import 'package:eatwise/constants/ew_colors.dart';
 import 'package:eatwise/constants/ew_styles.dart';
 import 'package:eatwise/models/company_item.dart';
+import 'package:eatwise/pages/corporate.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class CompanyContainer extends StatelessWidget {
   const CompanyContainer({
@@ -14,53 +16,74 @@ class CompanyContainer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 150,
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(20),
-        border: Border.all(
-          color: EWColors.lightgreen,
+        height: 150,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(20),
+          border: Border.all(
+            color: EWColors.lightgreen,
+          ),
         ),
-      ),
-      child: Column(
-        children: [
-          ClipRRect(
-            borderRadius: const BorderRadius.only(
-                topLeft: Radius.circular(20), topRight: Radius.circular(20)),
-            child: Image.asset(
-              width: double.infinity,
-              height: 75,
-              fit: BoxFit.fitWidth,
-              item.img,
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 8),
-            child: Column(
-              children: [
-                Row(
-                  children: [
-                    Text(
-                      item.title,
-                      style: EWTextStyles.headline
-                          .copyWith(color: EWColors.darkgreen),
+        child: Stack(children: [
+          MaterialButton(
+              onPressed: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (BuildContext context) => CorporatePage())),
+              child: Column(
+                children: [
+                  ClipRRect(
+                    borderRadius: const BorderRadius.only(
+                        topLeft: Radius.circular(20),
+                        topRight: Radius.circular(20)),
+                    child: Image.asset(
+                      width: double.infinity,
+                      height: 75,
+                      fit: BoxFit.fitWidth,
+                      item.img,
                     ),
-                  ],
-                ),
-                Row(
-                  children: [
-                    Text(
-                      item.subtitle,
-                      style:
-                          EWTextStyles.icon.copyWith(color: EWColors.darkgreen),
+                  ),
+                  Padding(
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 20, vertical: 8),
+                    child: Column(
+                      children: [
+                        Row(
+                          children: [
+                            Text(
+                              item.title,
+                              style: EWTextStyles.headline
+                                  .copyWith(color: EWColors.darkgreen),
+                            ),
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            Text(
+                              item.subtitle,
+                              style: EWTextStyles.icon
+                                  .copyWith(color: EWColors.darkgreen),
+                            ),
+                          ],
+                        ),
+                      ],
                     ),
-                  ],
+                  ),
+                ],
+              )),
+          Align(
+              alignment: const Alignment(0.8, 0),
+              child: Container(
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(300)),
+                child: const IconButton(
+                  icon: Icon(Icons.favorite_border),
+                  color: Colors.red,
+                  onPressed: null,
+                  iconSize: 50,
                 ),
-              ],
-            ),
-          ),
-        ],
-      ),
-    );
+              ))
+        ]));
   }
 }
