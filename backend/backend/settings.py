befdff856d4394f70190540eb19bd860a34597d2
Hector Bergman <hector.bergman.4575@student.uu.se>
2024-04-23 12:42:15 +0200

    k

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-)s-u@2y4ivw#azo79z+8=2-*^7g7$p^d)rh!@5ai!r6dlv!77r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["eb-django-app-dev.elasticbeanstalk.com"]
+ALLOWED_HOSTS = ["EatWise-dev-hb.eu-north-1.elasticbeanstalk.com"]
 
 
 # Application definition
