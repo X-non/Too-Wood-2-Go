5deee11c913e9c7f287cd39a0dcaa57b32a128c2
anec4889 <anton.eckervald.4889@student.uu.se>
2024-04-23 14:22:01 +0200

    Category functionality

--- frontend/lib/pages/shoppingcart_page.dart
+++ frontend/lib/pages/shoppingcart_page.dart
@@ -9,36 +9,6 @@ import 'package:eatwise/widgets/ew_shopping_cart_button.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-// Ta bort ---------
-const List<String> productName = [
-  'Bulle',
-  'Banan',
-  'Mjölk',
-];
-
-const List<String> oldPrice = [
-  '15 kr',
-  '10 kr',
-  '10 kr',
-];
-
-const List<String> newPrice = [
-  '10 kr',
-  '5 kr',
-  '7 kr',
-];
-
-final List<ProductItem> items = List<ProductItem>.generate(
-    3,
-    (index) => ProductItem(
-        img: 'assets/image/Gateau1.jpg',
-        name: productName[index],
-        priceOld: oldPrice[index],
-        priceNew: newPrice[index],
-        amount: 0));
-
-// -------------
-
 class ShoppingCartPage extends StatefulWidget {
   const ShoppingCartPage({super.key, required this.companyName});
 
